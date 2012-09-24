138bdd760976c9a12a24e55667a62f843ebad516
Halid Ozsoy <halidozsoy@hotmail.com>
2012-09-24 23:21:47 +0300

    Initial Commit

---
+++ WheelView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WheelView
+//
+//  Created by Halid Ozsoy on 9/24/12.
+//  Copyright (c) 2012 Halid Ozsoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WheelView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WheelView
+//
+//  Created by Halid Ozsoy on 9/24/12.
+//  Copyright (c) 2012 Halid Ozsoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
